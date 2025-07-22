9575c4f89fe280756cef37dee963d5d1f2dc588c
laxmijha3598 <laxmijha3598@gmail.com>
2025-07-22 17:07:15 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    document.getElementById('sidebar').classList.add('open');
+  }
+  
+  function closeMenu() {
+    document.getElementById('sidebar').classList.remove('open');
+  }
+  
